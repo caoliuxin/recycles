055783e6b6fa938b7e67798e24dd686d94b091bc
caoliuxin <r_f123@163.com>
2019-07-13 11:34:19 +0800

    曹刘鑫

--- src/main/java/com/aaa/controller/UserController.java
+++ src/main/java/com/aaa/controller/UserController.java
@@ -2,11 +2,21 @@ package com.aaa.controller;
 
 import com.aaa.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class UserController {
     @Resource
     private UserService service;
+    @RequestMapping("select")
+    public String selctUser(HttpServletRequest request){
+        List<Map<String,Object>> users=service.selectUser();
+        request.setAttribute("user",users);
+        return "selectUser";
+    }
 }
