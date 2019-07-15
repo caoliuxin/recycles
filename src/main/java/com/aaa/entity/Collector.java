1f1368418f535e6aedf8b851bfaa97ce76b63688
lww <13346836981@163.con>
2019-07-15 14:36:25 +0800

    和平安的实体

--- src/main/java/com/aaa/entity/Collector.java
+++ src/main/java/com/aaa/entity/Collector.java
@@ -6,7 +6,7 @@ public class Collector {
     private Integer id; //主键
     private String jobnum; //回收员工号
     private User userid; //用户id
-    private Integer collectorAuthenticateid; //认证id
+    private CollectorAuthenticate collectorAuthenticateid; //认证id
     private Integer state; //状态(0:在存 1：销毁)
 
     public Integer getId() {
@@ -33,11 +33,11 @@ public class Collector {
         this.userid = userid;
     }
 
-    public Integer getCollectorAuthenticateid() {
+    public CollectorAuthenticate getCollectorAuthenticateid() {
         return collectorAuthenticateid;
     }
 
-    public void setCollectorAuthenticateid(Integer collectorAuthenticateid) {
+    public void setCollectorAuthenticateid(CollectorAuthenticate collectorAuthenticateid) {
         this.collectorAuthenticateid = collectorAuthenticateid;
     }
 
@@ -49,7 +49,7 @@ public class Collector {
         this.state = state;
     }
 
-    public Collector(Integer id, String jobnum, User userid, Integer collectorAuthenticateid, Integer state) {
+    public Collector(Integer id, String jobnum, User userid, CollectorAuthenticate collectorAuthenticateid, Integer state) {
         this.id = id;
         this.jobnum = jobnum;
         this.userid = userid;
