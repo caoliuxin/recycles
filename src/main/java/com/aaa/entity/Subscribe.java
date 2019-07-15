72ba93c4dfb512cc5d9b3cc7a701b88e38e80ff3
caoliuxin <r_f123@163.com>
2019-07-15 10:42:19 +0800

    曹刘鑫

---
+++ src/main/java/com/aaa/entity/Subscribe.java
@@ -0,0 +1,94 @@
+package com.aaa.entity;
+
+public class Subscribe {
+    private Integer id;
+    private User userid;
+    private Address addressid;
+    private String subdate;
+    private Integer subquantumdate;
+    private String createtime;
+    private String cancletime;
+    private Integer orderstate;
+    private String canclreason;
+    private String remark;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUserid() {
+        return userid;
+    }
+
+    public void setUserid(User userid) {
+        this.userid = userid;
+    }
+
+    public Address getAddressid() {
+        return addressid;
+    }
+
+    public void setAddressid(Address addressid) {
+        this.addressid = addressid;
+    }
+
+    public String getSubdate() {
+        return subdate;
+    }
+
+    public void setSubdate(String subdate) {
+        this.subdate = subdate;
+    }
+
+    public Integer getSubquantumdate() {
+        return subquantumdate;
+    }
+
+    public void setSubquantumdate(Integer subquantumdate) {
+        this.subquantumdate = subquantumdate;
+    }
+
+    public String getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(String createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getCancletime() {
+        return cancletime;
+    }
+
+    public void setCancletime(String cancletime) {
+        this.cancletime = cancletime;
+    }
+
+    public Integer getOrderstate() {
+        return orderstate;
+    }
+
+    public void setOrderstate(Integer orderstate) {
+        this.orderstate = orderstate;
+    }
+
+    public String getCanclreason() {
+        return canclreason;
+    }
+
+    public void setCanclreason(String canclreason) {
+        this.canclreason = canclreason;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
